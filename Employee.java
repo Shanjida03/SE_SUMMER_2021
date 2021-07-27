c912b08cd6533d44610f2e6b0b4b3f8b34c07337
Shanjida03 <naynashanjida23@gmail.com>
2021-07-27 10:06:41 +0600

    First commit

---
+++ Employee.java
@@ -0,0 +1,29 @@
+package ClassPackages;
+
+public class Employee {
+    public String name;
+    public String empId;
+    public double salary;
+    
+    public void setName(String name){
+        this.name = name;
+    }
+    public void setEmpId(int empId){
+        this.empId = Integer.toString(id);
+    }
+    public void setSalary(double Salary){
+        this.salary = Salary;
+    }
+    
+    public String getName(){
+        return name;
+    }
+    public int getEmpId(){
+        int id = Integer.parseInt(empId);
+        return id;
+    }
+    public double getSalary(){
+        return salary;
+    }
+    
+}
